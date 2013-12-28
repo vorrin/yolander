0e163316fe280f5d8d08c1f07cec5dea18f106f3
Maurizio <vorrin.tal@gmail.com>
2013-12-28 16:49:35 +0000

    toyed around with making a little trials like bike mode

---
+++ Assets/Prefabs/BikeController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BikeController : MonoBehaviour {
+	public Rigidbody backWheel;
+	public Rigidbody frontWheel;
+	
+	[Range(0,100)] public float wheelMaxAngularVelocity;
+	// Use this for initialization
+	void Start () {
+		backWheel.rigidbody.maxAngularVelocity = wheelMaxAngularVelocity;
+		frontWheel.rigidbody.maxAngularVelocity = wheelMaxAngularVelocity;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButton(0)){
+			Debug.Log("PRESS");
+			Debug.Log(backWheel.rigidbody.maxAngularVelocity);
+			backWheel.AddTorque( -new Vector3(0,0f,1110f) * 100000f, ForceMode.Acceleration);
+		}
+	}
+}
